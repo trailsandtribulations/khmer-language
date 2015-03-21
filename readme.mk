f5c48ffb885087353cf03693239451983b85913c
cc "Maco" young <bangkokmaco@gmail.com>
2015-03-21 21:20:20 +0700

    initial

---
+++ readme.mk
@@ -0,0 +1,11 @@
+*** khmer-language ***
+
+browser-based flash card system for leaning languages.
+
+flash card data may be loaded as tab delimited file.
+
+flash card data may be changed by the app.
+
+data may also be stored long term in the browser.
+
+
